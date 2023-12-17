5a1b5044db9fdd29b888eae2b91fb39cf46072aa
Jay Bhuva <jaybhuva57@gmail.com>
2023-12-17 17:22:53 +0530

    new

---
+++ 02_basics/my_exp.js
@@ -0,0 +1,13 @@
+arr=[1,2,3,4,5,6]
+
+console.log("A ",arr);
+
+nar=arr.unshift(100)
+console.log("unshiftingggg");
+console.log("B ",nar);
+console.log("A ",arr);
+
+nar2=arr.shift()
+console.log("shiftingggg");
+console.log("C ",nar2);
+console.log("A ",arr);
