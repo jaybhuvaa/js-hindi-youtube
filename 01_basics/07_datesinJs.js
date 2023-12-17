8444c847f827a423013f5261e989b4e9be4b3477
Jay Bhuva <jaybhuva57@gmail.com>
2023-12-17 16:52:01 +0530

    done

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,9 +1,11 @@
 // Dates
 
-let myDate = new Date()
+// let myDate = new Date()
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
 // console.log(typeof myDate);
 
 // let myCreatedDate = new Date(2023, 0, 23)
@@ -18,15 +20,20 @@ let myTimeStamp = Date.now()
 // console.log(myCreatedDate.getTime());
 // console.log(Math.floor(Date.now()/1000));
 
-let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
+
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
 
 // `${newDate.getDay()} and the time `
+let newDate=new Date();
 
-newDate.toLocaleString('default', {
-    weekday: "long",
-    
-})
+
+console.log(newDate.toLocaleString('default',{
+    month :"narrow"
+}));
+
+console.log(newDate.toLocaleString('default',{
+    weekday :"long"
+}));
 
