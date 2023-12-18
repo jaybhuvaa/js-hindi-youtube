2d141b4b5f60c57e913c2702cc5d96de8d73fa3b
Jay Bhuva <jaybhuva57@gmail.com>
2023-12-18 23:13:48 +0530

    ok

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,7 +1,7 @@
 // for
 
-for (let i = 0; i <= 10; i++) {
-    const element = i;
+for (let j = 0; j <= 10; j++) {
+    const element = j;
     if (element == 5) {
         //console.log("5 is best number");
     }
